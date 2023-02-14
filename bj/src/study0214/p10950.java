0f7bc6517efd1dc2cec356af355c04836b8d6eef
Ssantoo <nowing0108@kakao.com>
2023-02-14 12:55:31 +0900

    feat: 알고리즘시작

---
+++ bj/src/study0214/p10950.java
@@ -0,0 +1,24 @@
+package study0214;
+
+import java.util.Scanner;
+
+public class p10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T, A, B;
+		
+		T = sc.nextInt();
+		
+		for(int i=0; i<T; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			
+			System.out.println(A+B);
+		}
+		
+
+	}
+
+}
